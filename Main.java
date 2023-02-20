45335b9e9917a4647e9d1259228425e7def8cc0b
tank2468 <s_eyeon@hotmail.com>
2023-02-19 19:25:27 -0700

    Update Main.java
    Testing commit and pushing

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
-public class Main {
-
-	
+public class Main {	
 public static void main (String[] args){
 	
 		Result();
-	
-	
+		System.out.Println("Testing that this is working as expected");
 }
 
-	public static int[] Result(){
 	
+	public static int[] Result()
+		{
 	
 		System.out.println("Please enter integers to be checked if any of the paired value adds up to 10 (separate it by commas)");
 	
@@ -23,15 +21,17 @@ public static void main (String[] args){
 		
 		for(int k = 0; k <div.length; k++){ //Storing elements into the array that was created
 			numbers[k] = Integer.parseInt(div[k]);
-			
 		}
 			int [] output = new int [numbers.length]; //Creating an array to store the results to be returned at the end
 			int counter=0; //Counter for storing correct pairs to be returned
 			
 			for (int i=0; i < numbers.length; i++){ //Loop that checks all the elements in the created array
 			
+				
 				int j = numbers.length - 1; //Start checking from the first element of the array with last element of the array
-				while( i < j){
+				while( i < j)
+					{
+					
 					int addedValue = numbers[i] + numbers[j]; //Add two i th and j th elements
 					
 					if (addedValue == 10){ //Compare to see if added integer is equal to ten; if so print out the pair
@@ -45,15 +45,11 @@ public static void main (String[] args){
 						else { //If the two pair do not add up to ten, check the value at loction of j-1
 							j--;
 						}
-				}
-						
+				}		
 			}
 					
 			input.close();
-			return output;
-					
-				
-				
+			return output;		
 	}		
 }
 
